1aa1f27b9e39a8b1223d38873bc83227f899f218
emVisible <1619346963@qq.com>
2024-02-15 23:17:35 +0800

    20240215

--- src/views/nav/emNav.tsx
+++ src/views/nav/emNav.tsx
@@ -1,25 +1,27 @@
 import data from '@/assets/data'
-import { Avatar, Col, Flex, Grid, Row } from 'antd'
+import { Avatar, Card, Col, Flex, Grid, Row } from 'antd'
 import { NavLink } from 'react-router-dom'
 
 export default () => {
   return (
-    <article id="magicFront" className="flex justify-center flex-1 bg-[#353B48] py-3">
+    <article id="magicFront" className="flex justify-center flex-1 bg-[#353B48] py-3 px-3 overflow-hidden">
       {data.map((item, index) => {
         return (
-          <Flex key={index} vertical gap="middle" className="flex-1 mx-3">
-            {item.map((subItem, subIndex) => {
-              return (
-                <Row className="flex-1 justify-center " key={subIndex}>
-                  <NavLink
-                    to={subItem.link}
-                    className=" flex justify-center items-center flex-1 p-0  bg-[#706FD3] rounded-lg transition-all hover:bg-[#9980FA] hover:text-3xl">
-                    <Avatar src={<img src={subItem.imgUrl} alt="avatar" />} />
-                    <p className="quota text-white font-mono transition-all">{subItem.name}</p>
-                  </NavLink>
-                </Row>
-              )
-            })}
+          <Flex key={index} gap="middle" className="flex-1 mx-3">
+            <Card className="flex flex-col flex-1 overflow-hidden bg-transparent border-none" title={<div>Title</div>}>
+              {item.map((subItem, subIndex) => {
+                return (
+                  <Row className="my-3 flex flex-1 justify-center" key={subIndex}>
+                    <NavLink
+                      to={subItem.link}
+                      className="py-10 flex justify-center items-center flex-1 p-0  bg-[#706FD3] rounded-lg transition-all hover:bg-[#9980FA] hover:scale-110">
+                      <Avatar shape="square" src={<img src={subItem.imgUrl} alt="avatar" />} />
+                      <p className="quota text-white font-mono transition-all">{subItem.name}</p>
+                    </NavLink>
+                  </Row>
+                )
+              })}
+            </Card>
           </Flex>
         )
       })}
